89a42d0a2d679e7850e36948fba83cfe6c7c9b19
SAMYSBL <samara.barcelos@live.com>
2025-05-27 15:12:50 -0300

    adicionando create table tarefas

--- controle_tarefas/database/db.sql
+++ controle_tarefas/database/db.sql
@@ -4,3 +4,10 @@ CREATE TABLE IF NOT EXISTS gerente (
     email TEXT NOT NULL UNIQUE,
     senha TEXT NOT NULL
 );
+CREATE TABLE tarefas (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    titulo VARCHAR(255),
+    descricao TEXT,
+    status VARCHAR(50),
+    colaborador_id INT
+);
