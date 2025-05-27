739ea7a99ef57a5ddfabd894f3b192cd2cdd926e
SAMYSBL <samara.barcelos@live.com>
2025-05-27 15:14:22 -0300

    criando classe tarefa

--- controle_tarefas/src/models/Tarefa.java
+++ controle_tarefas/src/models/Tarefa.java
@@ -1,5 +1,9 @@
 package models;
 
 public class Tarefa {
-    
+    private int id;
+    private String titulo;
+    private String descricao;
+    private String status;
+    private int colaboradorId;
 }
